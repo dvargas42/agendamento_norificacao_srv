40e338a3f231dc1604fc6004c45fad645b238cf8
dvargas42 <dvargas.eng@gmail.com>
2025-02-09 22:08:02 -0300

    feat: :tada: commit inicial

---
+++ src/main/java/com/dvargas42/agendamento_notificacao_srv/AgendamentoNotificacaoSrvApplication.java
@@ -0,0 +1,13 @@
+package com.dvargas42.agendamento_notificacao_srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoNotificacaoSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoNotificacaoSrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dvargas42/agendamento_notificacao_srv/AgendamentoNotificacaoSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvargas42.agendamento_notificacao_srv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoNotificacaoSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
